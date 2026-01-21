0bd2178fcef2981504b01dded2166ad7ab988420
Hernando Ramos Reyes <hramosreyes@apple.com>
2026-01-21 09:59:18 -0600

    B: created circleshape and player, added player_radius and line_width constants, drew player in the middle of the screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -9,7 +10,10 @@ def main():
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     while pygame:
         log_state()
         
@@ -18,7 +22,20 @@ def main():
                 return
         
         screen.fill("black")
+
+        # def Player(self):
+        #     def __init__(self, x = SCREEN_WIDTH // 2, y = SCREEN_HEIGHT // 2):
+        #         super().__init__(x, y)
+        #         self.rotation = 0
+
+        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+        player.draw(screen)
+
         pygame.display.flip()
+
+        dt = game_clock.tick(60) / 1000
+        # print(dt)
+                        
 
 if __name__ == "__main__":
     main()
