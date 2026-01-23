daee318285788b281760a0e210ba187079181679
Hernando Ramos Reyes <hramosreyes@apple.com>
2026-01-23 08:31:34 -0600

    L: Impleneted split mechanic, so when asteroid is shot it splits into two smaller asteroids. Small asteroids just disappear. Added split method to asteroids.py. Changed code in collision loop in main from previous commit to asteroid.split() instead of asteroid.kill()

--- asteroids.py
+++ asteroids.py
@@ -1,6 +1,8 @@
+import random
 import pygame
-from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS, LINE_WIDTH
 from circleshape import CircleShape
+from logger import log_event
 
 # Define the Asteroid class that inherits from CircleShape
 # The asteroid has a radius passed in the constructor
@@ -18,3 +20,22 @@ class Asteroid(CircleShape):
     # The velocity is a pygame.Vector2 object
     def update(self, dt):
         self.position += (self.velocity * dt)
+    
+    def split(self):
+        self.kill()
+        # Create two smaller asteroids at the same position
+        # Each smaller asteroid has half the radius of the original
+        # The smaller asteroids should move faster than the original
+        # The smaller asteroids should move in random directions
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            log_event("asteroid_split")
+            random_angle = random.uniform(20, 50)
+            new_asteroid_1_vector = self.velocity.rotate(random_angle)
+            new_asteroid_2_vector = self.velocity.rotate(-random_angle)
+            new_asteroid_radius = self.radius - ASTEROID_MIN_RADIUS
+            asteroid1 = Asteroid(self.position.x, self.position.y, new_asteroid_radius)
+            asteroid2 = Asteroid(self.position.x, self.position.y, new_asteroid_radius)
+            asteroid1.velocity = new_asteroid_1_vector * 1.2
+            asteroid2.velocity = new_asteroid_2_vector * 1.2
